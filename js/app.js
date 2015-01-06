27d8b88af56404523dde269bc39bcb365a0c7552
Tao Qian <taoqian_2015@depauw.edu>
2015-01-05 21:59:56 -0800

    Added carousel

--- js/app.js
+++ js/app.js
@@ -9,6 +9,27 @@ var main = function(){
     navigation:true
   });
 
+  $('#myCarousel').carousel({
+    interval: 40000
+  })
+
+  $('.carousel .item').each(function(){
+    var next = $(this).next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (var i=0;i<2;i++) {
+      next=next.next();
+      if (!next.length) {
+        next = $(this).siblings(':first');
+      }
+
+      next.children(':first-child').clone().appendTo($(this));
+    }
+  });
+
 }
 
 $(document).ready(main);
